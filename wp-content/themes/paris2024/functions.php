64e504febe02e8ce8eda71ade1e22e6b1ee936c5
thibobbs <thibaut.renard@hetic.net>
2017-10-22 18:50:29 +0200

    :ambulance: assets -> dist

--- wp-content/themes/paris2024/functions.php
+++ wp-content/themes/paris2024/functions.php
@@ -4,9 +4,9 @@ define( 'THEME_PATH' ,          get_template_directory()            );
 //define( 'TEMPLATE_PATH' ,       THEME_PATH .    '/templates'        );
 
 define( 'THEME_URL' ,           get_template_directory_uri()        );
-define( 'CSS_URL' ,             THEME_URL .    '/assets/styles'       );
-define( 'IMAGES_URL' ,          THEME_URL .    '/assets/images'       );
-define( 'JS_URL' ,              THEME_URL .    '/assets/scripts'      );
+define( 'CSS_URL' ,             THEME_URL .    '/dist/styles'       );
+define( 'IMAGES_URL' ,          THEME_URL .    '/dist/images'       );
+define( 'JS_URL' ,              THEME_URL .    '/dist/scripts'      );
 //define( 'FAVICONS_URL' ,        THEME_URL .    '/dist/favicon'      );
 //define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
 
