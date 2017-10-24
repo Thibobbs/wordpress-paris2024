f551559525d0bbbac2f96696a286b4503e28e0c8
JohannDesobry <jo.desobry@gmail.com>
2017-10-25 00:07:49 +0200

    :lipstick: start design concept page

--- wp-content/themes/paris2024/page-templates/concept.php
+++ wp-content/themes/paris2024/page-templates/concept.php
@@ -3,4 +3,14 @@
 * Template name: Concept
 */
 ?>
-<h1>Concept.php</h1>
+
+<?php get_header() ?>
+
+<div class="video-intro">
+    <div class="video-intro__page-name">Concept</div>
+    <h1 class="video-intro__title">Paris, parc olympique</h1>
+    <p class="video-intro__paragraph">Notre projet comprend des sites existants ou temporaires à hauteur de 95% et chacun d’entre eux présente la perspective d’un héritage défini précis et respectueux des plans de développement de la ville à long terme.</p>
+    <button class="video-intro__button">Play</button>
+</div>
+
+<?php get_footer() ?>
