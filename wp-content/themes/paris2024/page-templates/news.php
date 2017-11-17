bdffa9f5ba2ad97fc5295c94a272096e966d6bbc
Bérénice David <berenice.david@hotmail.fr>
2017-11-17 20:38:00 +0100

    :construction: WIP News page

--- wp-content/themes/paris2024/page-templates/news.php
+++ wp-content/themes/paris2024/page-templates/news.php
@@ -3,4 +3,72 @@
 * Template name: Actualités
 */
 ?>
-<h1>News.php</h1>
+<?php get_header(); ?>
+
+<div class="image-intro">
+    <h1 class="image-intro__title">Toutes les actualités</h1>
+    <div class="image-intro__overlay"></div>
+    <div class="image-intro__background"></div>
+</div>
+<div class="search">
+    <div class="search__input"></div>
+    <div class="search__button"></div>
+</div>
+<div class="news">
+    <div class="news__wrapper">
+        <div class="news__categories">
+            <a href="#" title="">Tous</a>
+            <a href="#" title="">Actualités du CIO</a>
+            <a href="#" title="">Actualités olympiques</a>
+            <a href="#" title="">Élection des villes hôtes</a>
+            <a href="#" title="">Paris 2024</a>
+        </div>
+        <div class="news__articles">
+            <div class="articles__article">
+                <div class="article__thumbnail">
+                    <img src="" alt="">
+                </div>
+                <div class="article__content">
+                    <div class="article__title">Les athlètes au cœur des Jeux Olympiques</div>
+                    <div class="article__text">Les athlètes sont au cœur des Jeux Olympiques. Sans eux, les Jeux ne pourraient pas avoir lieu et leur expérience est inestimable pour une...</div>
+                </div>
+                <div class="article__data">
+                    <div class="article__data-date">10 Juil. 2017</div>
+                    <div class="article__data-hastag">#Paris2024</div>
+                </div>
+            </div>
+            
+            <div class="articles__article">
+                <div class="article__thumbnail">
+                    <img src="" alt="">
+                </div>
+                <div class="article__content">
+                    <div class="article__title">Les athlètes au cœur des Jeux Olympiques</div>
+                    <div class="article__text">Les athlètes sont au cœur des Jeux Olympiques. Sans eux, les Jeux ne pourraient pas avoir lieu et leur expérience est inestimable pour une...</div>
+                </div>
+                <div class="article__data">
+                    <div class="article__data-date">10 Juil. 2017</div>
+                    <div class="article__data-hastag">#Paris2024</div>
+                </div>
+            </div>
+
+            <div class="articles__article">
+                <div class="article__thumbnail">
+                    <img src="" alt="">
+                </div>
+                <div class="article__content">
+                    <div class="article__title">Les athlètes au cœur des Jeux Olympiques</div>
+                    <div class="article__text">Les athlètes sont au cœur des Jeux Olympiques. Sans eux, les Jeux ne pourraient pas avoir lieu et leur expérience est inestimable pour une...</div>
+                </div>
+                <div class="article__data">
+                    <div class="article__data-date">10 Juil. 2017</div>
+                    <div class="article__data-hastag">#Paris2024</div>
+                </div>
+            </div>
+        </div>
+        <div class="news__button">
+            <a class="news__see-more" href="#" title="Voir plus">Voir plus</a>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?> 
