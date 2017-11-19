5c22533054659f20744e61810f6c9827beff263b
thibobbs <thibaut.renard@hetic.net>
2017-11-19 23:30:05 +0100

    :construction: home.php & scss & js

--- wp-content/themes/paris2024/assets/javascript/main.js
+++ wp-content/themes/paris2024/assets/javascript/main.js
@@ -1,20 +1,26 @@
-const vid = document.querySelector('.video-intro__player video'),
-fade      = document.querySelector('.video-intro__fade-effect'),
-btn_play  = document.querySelector('.video-intro__button')
+if (document.querySelector('body').classList.contains('page-id-5') || document.querySelector('body').classList.contains('page-id-7')) {
+  const vid = document.querySelector('.video-intro__player video'),
+    fade = document.querySelector('.video-intro__fade-effect'),
+    btn_play = document.querySelector('.video-intro__button')
 
-btn_play.addEventListener('click', (e) =>
-{
-	e.preventDefault()
-	vid.style.display = 'block'
-	if (vid.paused)
-	{
-		fade.classList.add('active')
-		vid.play()
-	}
-	else 
-	{	
-		fade.classList.remove('active')
-		vid.pause()
-	}
-})
+  btn_play.addEventListener('click', (e) => {
+    e.preventDefault()
+    vid.style.display = 'block'
+    if (vid.paused) {
+      fade.classList.add('active')
+      vid.play()
+    } else {
+      fade.classList.remove('active')
+      vid.pause()
+    }
+  })
+}
 
+if (document.querySelector('body').classList.contains('home')) {
+  const slider = document.querySelector('.slider')
+  const slides = Array.prototype.slice.call(document.querySelectorAll('.slider__slide__filter'))
+  slider.addEventListener('click', (event) => {
+    console.log(slides.indexOf(event.target))
+    slider.className = 'slider slider--slide' + (slides.indexOf(event.target) + 1)
+  })
+}
--- wp-content/themes/paris2024/home.php
+++ wp-content/themes/paris2024/home.php
@@ -3,15 +3,41 @@
 <div id="content">
 
     <div class="slider">
+       
+       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+       
+        <div class="slider__slide">
+            <div class="slider__slide__filter" style="background:url('<?= the_post_thumbnail_url(); ?>;')">>
+                <div class="slider__slide__container">
+                    <h2 class="slider__slide__article-type"><?php the_category(', '); ?></h2>
+                    <h1 class="slider__slide__title"><?php the_title(); ?></h1>
+                    <a href="<?php the_permalink(); ?>" class="btn--blue btn__text--white slider__slide__button">Voir l'article</a>
+                </div>
+            </div>
+        </div>
         <div class="slider__slide">
             <div class="slider__slide__filter">
                 <div class="slider__slide__container">
-                    <h2 class="slider__slide__article-type">Développement territorial</h2>
-                    <h1 class="slider__slide__title">Des Jeux de partage, plus inclusifs et solidaires</h2>
+                    <h2 class="slider__slide__article-type">Développement territorial2</h2>
+                    <h1 class="slider__slide__title">Des Jeux de partage, plus inclusifs et solidaires</h1>
                     <a href="#" class="btn--blue btn__text--white slider__slide__button">Voir l'article</a>
                 </div>
             </div>
         </div>
+        <div class="slider__slide">
+            <div class="slider__slide__filter">
+                <div class="slider__slide__container">
+                    <h2 class="slider__slide__article-type">Développement territorial2</h2>
+                    <h1 class="slider__slide__title">Des Jeux de partage, plus inclusifs et solidaires</h1>
+                    <a href="#" class="btn--blue btn__text--white slider__slide__button">Voir l'article</a>
+                </div>
+            </div>
+        </div>
+
+      <?php endwhile; else: ?>
+      <p>Désolé, le site semble rencontrer un problème ... Merci de revenir ultérieurement.</p>
+      <?php endif; ?>
+
     </div>
 
     <div class="discover">
@@ -23,6 +49,24 @@
             <p class="discover__text">Depuis des centaines d'années, Paris accueuille tous les peuples de la Terre, y compris les pères fondateurs du Mouvement olympique, dans un esprit de collaboration et d'inspiration mutuelle, pour générer des idées et façonner l'avenir</p>
             <a href="#" class="discover__link">Découvrir</a>
         </div>
+    </div>
+
+    <div class="partners">
+      <ul>
+        <li class="partner"><img src="" alt=""></li>
+        <li class="partner"><img src="" alt=""></li>
+        <li class="partner"><img src="" alt=""></li>
+        <li class="partner"><img src="" alt=""></li>
+        <li class="partner"><img src="" alt=""></li>
+      </ul>
+    </div>
+
+    <div class="athlete">
+      <div class="athlete__txt">
+        <h3 class="athlete__name">Teddy Rinner</h3>
+        <p class="athlete__caption">La jeunessa va se préparer à accueillir les jeux.</p>
+        <a href="" class="athlete__button btn--white btn__text--blue">Le judo</a>
+      </div>
     </div>
 
 </div>
