34e43dcae251f9f5edb4ea834b9313ca7dd3a4a6
thibobbs <thibaut.renard@hetic.net>
2017-11-27 23:38:40 +0100

    :sparkles: slider concept.php

--- wp-content/themes/paris2024/assets/javascript/main.js
+++ wp-content/themes/paris2024/assets/javascript/main.js
@@ -94,3 +94,88 @@ if (document.querySelector('body').classList.contains('home')) {
     autoSlide = setInterval(next, 5000)
   })
 }
+
+if (document.querySelector('body').classList.contains('page-id-5')) {
+  let i = 0
+  const slider = document.querySelector('.slider-concept')
+  const slides_container = slider.querySelector('.slider__slides')
+  let slides = [].slice.call(slider.querySelectorAll('.slider__slide'))
+  const controllers = slider.querySelector('.slider__controllers')
+  const prev_button = controllers.querySelector('.controllers__prev')
+  const next_button = controllers.querySelector('.controllers__next')
+  const tiles_container = controllers.querySelector('.controllers__tiles')
+  const tiles = [].slice.call(tiles_container.querySelectorAll('.tile'))
+
+  slides_container.style.width = (slides.length * 50) + '%'
+  slides_container.style.transform = 'translate(calc(-50%/' + slides.length + '))'
+  
+  slides[1].className += ' slider__slide--active'
+
+  prev_button.addEventListener('click', (e) => {
+    e.preventDefault()
+    previous()
+  })
+
+  next_button.addEventListener('click', (e) => {
+    e.preventDefault()
+    next()
+  })
+
+  tiles_container.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (tiles[tiles.indexOf(e.target)] !== undefined && !tiles[tiles.indexOf(e.target)].classList.contains('tile--active')) {
+      tiles_container.querySelector('.tile--active').classList.remove('tile--active')
+      e.target.className += ' tile--active'
+
+      const place = slides.indexOf(slides_container.querySelector('[index="' + tiles.indexOf(e.target) + '"]'))
+      if (place < 1) {
+        previous()
+      }
+      else if (place > 1) {
+        next()
+      }
+    }
+  })
+  
+  function previous() {
+    slides_container.querySelector('.slider__slide--active').classList.remove('slider__slide--active')
+    slides[0].className += ' slider__slide--active'
+    slides_container.style.transform = 'translate(calc((-50%/' + slides.length + ') + (2* (50%/' + slides.length + ')))'
+    setTimeout(() => {
+      slides_container.className += ' slider__slides--return'
+      slides_container.style.transform = 'translate(calc(-50%/' + slides.length + '))'
+      slides[0].before(slides[slides.length-1])
+      slides = [].slice.call(slider.querySelectorAll('.slider__slide'))
+    }, 300)
+    slides_container.classList.remove('slider__slides--return')
+
+    tiles_container.querySelector('.tile--active').classList.remove('tile--active')
+    tiles[slides[0].getAttribute('index')].className += ' tile--active'
+  }
+  
+  function next() {
+    slides_container.querySelector('.slider__slide--active').classList.remove('slider__slide--active')
+    slides[2].className += ' slider__slide--active'
+    slides_container.style.transform = 'translate(calc((-50%/' + slides.length + ') - (2* (50%/' + slides.length + ')))'
+    setTimeout(() => {
+      slides_container.className += ' slider__slides--return'
+      slides_container.style.transform = 'translate(calc(-50%/' + slides.length + '))'
+      slides[slides.length-1].after(slides[0])
+      slides = [].slice.call(slider.querySelectorAll('.slider__slide'))
+    }, 300)
+    slides_container.classList.remove('slider__slides--return')
+
+    tiles_container.querySelector('.tile--active').classList.remove('tile--active')
+    tiles[slides[2].getAttribute('index')].className += ' tile--active'
+  }
+  
+//  let autoSlide = setInterval(next, 5000)
+//  
+//  slider.addEventListener("mouseover", () => {
+//    clearInterval(autoSlide)
+//  })
+//  
+//  slider.addEventListener("mouseout", () => {
+//    autoSlide = setInterval(next, 5000)
+//  })
+}
--- wp-content/themes/paris2024/page-templates/concept.php
+++ wp-content/themes/paris2024/page-templates/concept.php
@@ -71,3 +71,61 @@
     ?>
     
 </div>
+
+
+<div class="slider-concept">
+  <div class="slider__name">SITES</div>
+  <div class="slider__slides">
+
+    <?php
+
+          $button_txt = get_field('home_slider_button_text');
+
+          $args = array(
+            'posts_per_page' => '3'
+          );
+          // The Query
+          $the_query = new WP_Query( $args );
+
+          // The Loop
+          if ( $the_query->have_posts() ) {
+            $i = 0;
+            while ( $the_query->have_posts() ) {
+              $the_query->the_post();
+        ?>
+
+      <div class="slider__slide" index="<?= $i ?>">
+        <div class="slide__info">
+          <h3 class="slide__category">
+            <?php the_category(', '); ?>
+          </h3>
+          <h2 class="slide__title">
+            <?php the_title(); ?>
+          </h2>
+        </div>
+      </div>
+
+      <?php
+              $i = ($i + 1);
+            }
+            wp_reset_postdata();
+          } else {
+            echo '<p>Désolé, le site semble rencontrer un problème ... Merci de revenir ultérieurement.</p>';
+          }
+      ?>
+
+  </div>
+
+  <div class="slider__controllers">
+    <a href="" class="controllers controllers__prev"><img src="<?= IMAGES_URL ?>/arrows/left-arrow.svg" alt=""></a>
+    <a href="" class="controllers controllers__next"><img src="<?= IMAGES_URL ?>/arrows/right-arrow.svg" alt=""></a>
+    <div class="controllers__tiles">
+      <div class="tile"></div>
+      <div class="tile tile--active"></div>
+      <div class="tile"></div>
+    </div>
+  </div>
+
+</div>
+
+<?php get_footer() ?>
