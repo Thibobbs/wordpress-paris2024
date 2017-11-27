3b4309e6fbf88866b861066555b4c983a1abc889
JohannDesobry <jo.desobry@gmail.com>
2017-11-27 18:58:20 +0100

    vision fix video

--- wp-content/themes/paris2024/page-templates/vision.php
+++ wp-content/themes/paris2024/page-templates/vision.php
@@ -6,24 +6,25 @@
 
 <?php get_header() ?>
 
-<div class="video-intro">
-  <div class="video-intro__page-name"><?php the_field('page_name'); ?></div>
-  <h1 class="video-intro__title"><?php the_field('vision_title'); ?></h1>
-  <p class="video-intro__paragraph"><?php the_field('vision_text'); ?></p>
-  <button class="video-intro__button">
+<section class="video-intro">
+    <div class="video-intro__page-name"><?php the_field("page_name")?></div>
+    <h1 class="video-intro__title"><?php the_field("vision_title")?></h1>
+    <p class="video-intro__paragraph"><?php the_field("vision_text")?></p>
+    <button class="video-intro__button">
         <svg fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 5v14l11-7z"/>
             <path d="M0 0h24v24H0z" fill="none"/>
         </svg>
     </button>
-  <div class="video-intro__fade-effect"></div>
-  <div class="video-intro__player">
-    <video>
+    <div class="video-intro__fade-effect"></div>
+    <div class="video-intro__player">
+        <video>
           <source src="<?= UPLOADS.'/concept.mp4' ?>" type="video/mp4">
           Your browser does not support the video tag.
         </video>
-  </div>
-</div>
+    </div>
+    <div class="video-intro__bg"></div>
+</section>
 
 <div class="description-vision">
   <div class="description-vision__txt">
