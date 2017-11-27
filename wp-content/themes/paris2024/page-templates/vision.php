f50a232fd72c2d83befba3c30f21675d5671864c
thibobbs <thibaut.renard@hetic.net>
2017-11-27 17:53:01 +0100

    :sparkles: vision.php with acf

--- wp-content/themes/paris2024/page-templates/vision.php
+++ wp-content/themes/paris2024/page-templates/vision.php
@@ -7,9 +7,9 @@
 <?php get_header() ?>
 
 <div class="video-intro">
-  <div class="video-intro__page-name">Vision</div>
-  <h1 class="video-intro__title">Ensemble, célébrons la victoire</h1>
-  <p class="video-intro__paragraph">Paris 2024 proposera une nouvelle vision de l’Olympisme en action, dans un esprit unique de célébration internationale.</p>
+  <div class="video-intro__page-name"><?php the_field('vision'); ?></div>
+  <h1 class="video-intro__title"><?php the_field('vision_title'); ?></h1>
+  <p class="video-intro__paragraph"><?php the_field('vision_text'); ?></p>
   <button class="video-intro__button">
         <svg fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 5v14l11-7z"/>
@@ -27,23 +27,23 @@
 
 <div class="description-vision">
   <div class="description-vision__txt">
-    <h4 class="txt__title">Un projet en héritage</h4>
-    <p class="txt__paragraph">Le village olympique est conçu comme un projet exceptionnel de rénovation urbaine, il deviendra une référence majeure en termes de durabilité et de rénovation urbaine.</p>
+    <h4 class="txt__title"><?php the_field('vision_stats_title'); ?></h4>
+    <p class="txt__paragraph"><?php the_field('vision_stats_text'); ?></p>
     <div class="description-vision__cards">
       <div class="cards card-spectators">
         <img src="" alt="" class="cards__img">
-        <p class="cards__number">100%</p>
-        <p class="cards__txt">des spectateurs se rendant sur les sites, par transports publics, à pied ou à bicyclette</p>
+        <p class="cards__number"><?php the_field('vision_stats_1_number'); ?></p>
+        <p class="cards__txt"><?php the_field('vision_stats_1_text'); ?></p>
       </div>
       <div class="cards card-cities">
         <img src="" alt="" class="cards__img">
-        <p class="cards__number">85%</p>
-        <p class="cards__txt">des grandes villes de France à Paris en moins de quatre heures</p>
+        <p class="cards__number"><?php the_field('vision_stats_2_number'); ?></p>
+        <p class="cards__txt"><?php the_field('vision_stats_2_text'); ?></p>
       </div>
       <div class="cards card-transports">
         <img src="" alt="" class="cards__img">
-        <p class="cards__number">0€</p>
-        <p class="cards__txt">c'est le prix du ticket des transports parisiens pour les détenteurs de billets pour les jeux</p>
+        <p class="cards__number"><?php the_field('vision_stats_3_number'); ?></p>
+        <p class="cards__txt"><?php the_field('vision_stats_3_text'); ?></p>
       </div>
     </div>
   </div>
@@ -51,9 +51,9 @@
 
 <div class="athlete">
   <div class="athlete__txt">
-    <h3 class="athlete__name">Tony yoka</h3>
-    <p class="athlete__caption">La boxe à Roland-Garros, c'est géant !</p>
-    <a href="" class="athlete__button btn--white btn__text--blue">La boxe</a>
+    <h3 class="athlete__name"><?php the_field('vision_athlete'); ?></h3>
+    <p class="athlete__caption"><?php the_field('vision_athlete_caption'); ?></p>
+    <a href="" class="athlete__button btn__main btn__main--white btn__text btn__text--blue"><?php the_field('vision_athlete_button'); ?></a>
   </div>
 </div>
 
