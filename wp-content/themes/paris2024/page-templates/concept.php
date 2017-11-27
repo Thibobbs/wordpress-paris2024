4169b99503e3851afa9436bbc57addaefd5e3acb
JohannDesobry <jo.desobry@gmail.com>
2017-11-27 18:19:06 +0100

    concept section improve responsive + sports list

--- wp-content/themes/paris2024/page-templates/concept.php
+++ wp-content/themes/paris2024/page-templates/concept.php
@@ -4,12 +4,19 @@
 */
 ?>
 
+<!-- <video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" playsinline autoplay muted loop>
+    <source src="http://thenewcode.com/assets/videos/polina.webm" type="video/webm">
+    <source src="http://thenewcode.com/assets/videos/polina.mp4" type="video/mp4">
+</video> -->
+
+<!-- -->
+
 <?php get_header() ?>
 
-<div class="video-intro">
-    <div class="video-intro__page-name">Concept</div>
-    <h1 class="video-intro__title">Paris, parc olympique</h1>
-    <p class="video-intro__paragraph">Notre projet comprend des sites existants ou temporaires à hauteur de 95% et chacun d’entre eux présente la perspective d’un héritage défini précis et respectueux des plans de développement de la ville à long terme.</p>
+<section class="video-intro">
+    <div class="video-intro__page-name"><?php the_field("page_name")?></div>
+    <h1 class="video-intro__title"><?php the_field("concept_title")?></h1>
+    <p class="video-intro__paragraph"><?php the_field("concept_text")?></p>
     <button class="video-intro__button">
         <svg fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 5v14l11-7z"/>
@@ -23,51 +30,62 @@
           Your browser does not support the video tag.
         </video>
     </div>
-</div>
+    <div class="video-intro__bg"></div>
+</section>
 
-<div class="description-project">
+<section class="description-project">
     <div class="description-project__txt">
-        <h3 class="description-project__title">Un projet en héritage</h2>
-        <p class="description-project__paragraph">Le village olympique est conçu comme un projet exceptionnel de rénovation urbaine, il deviendra une référence majeure en termes de durabilité et de rénovation urbaine.</p>
+        <h3 class="description-project__title"><?php the_field("concept_project") ?></h3>
+        <p class="description-project__paragraph"><?php the_field("concept_project_text") ?></p>
     </div>
-</div>
+</section>
 
-<div class="list-sports">
+<section class="list-sports">
 
-    <?php
-    $args= array(
-    'post_type' => 'sport',
-    'posts_per_page' => 20
-    );
-
-    $the_query = new WP_Query( $args );
-
-    // Start Loop
-    if ( $the_query->have_posts() ) {
-        while ( $the_query->have_posts() ) {
-            $the_query->the_post();
-    ?>
-
-    <div class="list-sports__item">
-        <?php the_title() ?>
-        <div class="thumbnail">
-            <?php
-              if(has_post_thumbnail())
-              {
-                the_post_thumbnail("hub_sport_thumbnail");
-              }
-            ?>
-        </div>
-    </div>
-
-     <!-- End loop -->
-    <?php
-    }
-       /* Restore original Post Data */
-       wp_reset_postdata();
-    }
-    ?>
+    <h3 class="list-sports__title"><?php the_field("concept_sports_title") ?></h3>
+    <p class="list-sports__text"><?php the_field("concept_sports_text") ?></p>
     
-</div>
+    <div class="list-sports__items">
+        <?php
+        $args= array(
+        'post_type' => 'sport',
+        'posts_per_page' => 20
+        );
+
+        $the_query = new WP_Query( $args );
+
+        // Start Loop
+        if ( $the_query->have_posts() ) {
+            while ( $the_query->have_posts() ) {
+                $the_query->the_post();
+        ?>
+
+        <div class="list-sports__item">
+            <div class="item__fade"></div>
+            <h3 class="item__title"><?php the_title() ?></h3>
+            <div class="thumbnail">
+                <?php
+                  if(has_post_thumbnail())
+                  {
+                    the_post_thumbnail("hub_sport_thumbnail");
+                  }
+                ?>
+            </div>
+        </div>
+
+         <!-- End loop -->
+        <?php
+        }
+           /* Restore original Post Data */
+           wp_reset_postdata();
+        }
+        ?>
+    </div>
+    
+</section>
+
+<section class="sites">
+     <h3 class="sites__title"><?php the_field("concept_sites_title") ?></h3>
+</section>
 
 <?php get_footer() ?>
