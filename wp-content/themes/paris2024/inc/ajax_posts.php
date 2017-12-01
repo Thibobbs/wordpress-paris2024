b4b7836b0f74c16d8f5892feae0408312f44d561
naim-ea <elayadinaim@gmail.com>
2017-12-01 01:02:25 +0100

    :WIP: AJAX load more bug

--- wp-content/themes/paris2024/inc/ajax_posts.php
+++ wp-content/themes/paris2024/inc/ajax_posts.php
@@ -8,15 +8,13 @@ function ajax_fetch() {
         if (document.querySelector('body').classList.contains('.page-id-9')) {
           var number_of_pages = document.querySelector('.news__button').getAttribute('pages');
           number_of_pages--;
-          console.log(number_of_pages);
         }
         function fetch(element){
-
             if(element.tagName == 'DIV'){
 
                 if(element.classList.contains('news__category')){
                     document.querySelector('.news__loader').style.display="block";
-                    document.querySelector('.news__articles').innerHTML = "";
+                    document.querySelector('.stories__container').innerHTML = "";
 
                     for(i = 0; i < document.querySelectorAll('.news__category').length; i++){
                         document.querySelectorAll('.news__category')[i].classList.remove('active');
@@ -31,7 +29,7 @@ function ajax_fetch() {
                             data: { action: 'search_and_filter_data_fetch'},
                             success: function(data) {
                                 document.querySelector('.news__loader').style.display="none";
-                                jQuery('.news__articles').html( data );
+                                document.querySelector('.stories__container').innerHTML = data;
                             }
                         });
                     }
@@ -43,7 +41,7 @@ function ajax_fetch() {
                             data: { action: 'search_and_filter_data_fetch', taxonomy: element.getAttribute('category') },
                             success: function(data) {
                                 document.querySelector('.news__loader').style.display="none";
-                                jQuery('.news__articles').html( data );
+                                document.querySelector('.stories__container').innerHTML = data;
                             }
                         });   
                     }
@@ -59,9 +57,9 @@ function ajax_fetch() {
                         data: { action: 'load_more_data_fetch', page: page},
                         success: function(data) {
                             if(number_of_pages == 0){
-                                jQuery('.news__button').remove();
+                                document.querySelector('.news__button').remove();
                             }
-                            jQuery('.news__articles').append( data );
+                            document.querySelector('.stories__container').innerHTML = data;
                             
                         }
                     });
@@ -69,7 +67,7 @@ function ajax_fetch() {
             }
             else{
                 document.querySelector('.news__loader').style.display="block";
-                document.querySelector('.news__articles').innerHTML = "";
+                document.querySelector('.stories__container').innerHTML = "";
 
                 console.log("input");
                 jQuery.ajax({
@@ -78,7 +76,7 @@ function ajax_fetch() {
                     data: { action: 'search_and_filter_data_fetch', keyword: jQuery('#keyword').val() },
                     success: function(data) {
                         document.querySelector('.news__loader').style.display="none";
-                        jQuery('.news__articles').html( data );
+                        document.querySelector('.stories__container').innerHTML = data;
                     }
                 });
             }
@@ -96,7 +94,7 @@ function search_and_filter_data_fetch(){
 
     if(isset($_POST['taxonomy'])){
         $args =  array( 
-            'posts_per_page' => -1, 
+            'posts_per_page' => 9, 
             's' => esc_attr( $_POST['keyword'] ), 
             'post_type' => 'post',
             'tax_query' => array(
@@ -110,7 +108,7 @@ function search_and_filter_data_fetch(){
     }
     else{
         $args =  array( 
-            'posts_per_page' => -1, 
+            'posts_per_page' => 9, 
             's' => esc_attr( $_POST['keyword'] ), 
             'post_type' => 'post'
         );
@@ -118,33 +116,33 @@ function search_and_filter_data_fetch(){
 
     $the_query = new WP_Query( $args );
     if( $the_query->have_posts() ) :
-        while( $the_query->have_posts() ): $the_query->the_post(); ?>
+        while( $the_query->have_posts() ): 
+            $the_query->the_post(); 
+            ?>
+            <a class="stories" href="<?php the_permalink() ?>">
+                <div class="stories__link <?= get_the_terms($post->id, 'category')[0]->slug ?>">
 
-            <div class="articles__article">
-                <a href="<?php the_permalink() ?>">
-                    <div class="article__thumbnail">
-                        <?php
-                        if(has_post_thumbnail())
-                        {
-                            the_post_thumbnail("hub_post_thumbnail");
-                        }
-                        ?>
+                    <div class="stories__link--thumbnail">
+                        <?php if(has_post_thumbnail()) { the_post_thumbnail("hub_post_thumbnail"); } ?>
                         <div class="thumbnail__line"></div>
                     </div>
-                    <div class="article__content">
-                        <div class="article__title"><?php the_title() ?></div>
-                        <div class="article__text">Les athlètes sont au cœur des Jeux Olympiques. Sans eux, les Jeux ne pourraient pas avoir lieu et leur expérience est inestimable pour une...</div>
-                    </div>
-                    <div class="article__data-wrapper">
-                        <div class="article__line"></div>
-                        <div class="article__data">
-                            <div class="article__data-date">10 Juil. 2017</div>
-                            <div class="article__data-hastag">#Paris2024</div>
+
+                    <div class="stories__content">
+                        <div class="stories__content--text">
+                            <h4><?php the_title() ?></h4>
+                            <p class="stories__link--excerpt">
+                                <?php echo custom_field_excerpt(); ?>
+                            </p>
+                        </div>
+                        <div class="stories__content--infos">
+                            <p><?php the_field('article_date'); ?></p>
+                            <p>#<?= get_the_terms($post->id, 'category')[0]->slug ?></p>
                         </div>
                     </div>
-                </a>
-            </div>
 
+                </div>
+            </a>
+            <!-- End loop -->
         <?php endwhile;
         wp_reset_postdata();  
     endif;
@@ -159,43 +157,45 @@ function load_more_data_fetch(){
 
     $paged = $_POST['page'];    
     $args =  array( 
-        'posts_per_page' => 2, 
+        'posts_per_page' => 9, 
         'post_type' => 'post',
         'paged' => $paged,        
     );
 
     $load_more_query = new WP_Query( $args );
     if( $load_more_query->have_posts() ) :
-        while( $load_more_query->have_posts() ): $load_more_query->the_post(); ?>
+        while( $load_more_query->have_posts() ): $load_more_query->the_post(); 
+            $wp_query->the_post();
+            ?>
+            <a class="stories" href="<?php the_permalink() ?>">
+                <div class="stories__link <?= get_the_terms($post->id, 'category')[0]->slug ?>">
 
-            <div class="articles__article">
-                <a href="<?php the_permalink() ?>">
-                    <div class="article__thumbnail">
-                        <?php
-                            if(has_post_thumbnail())
-                            {
-                                the_post_thumbnail("hub_post_thumbnail");
-                            }
-                        ?>
+                    <div class="stories__link--thumbnail">
+                        <?php if(has_post_thumbnail()) { the_post_thumbnail("hub_post_thumbnail"); } ?>
                         <div class="thumbnail__line"></div>
                     </div>
-                    <div class="article__content">
-                        <div class="article__title"><?php the_title() ?></div>
-                        <div class="article__text"><?php the_field('article_intro'); ?></div>
-                    </div>
-                    <div class="article__data-wrapper">
-                        <div class="article__line"></div>
-                        <div class="article__data">
-                            <div class="article__data-date"><?php the_field('article_date'); ?></div>
-                            <div class="article__data-hastag">#Paris2024</div>
+
+                    <div class="stories__content">
+                        <div class="stories__content--text">
+                            <h4><?php the_title() ?></h4>
+                            <p class="stories__link--excerpt">
+                                <?php echo custom_field_excerpt(); ?>
+                            </p>
+                        </div>
+                        <div class="stories__content--infos">
+                            <p><?php the_field('article_date'); ?></p>
+                            <p>#<?= get_the_terms($post->id, 'category')[0]->slug ?></p>
                         </div>
                     </div>
-                </a>
-            </div>
 
+                </div>
+            </a>
+        <!-- End loop -->
         <?php
         endwhile;
-        wp_reset_postdata();
+    /* Restore original Post Data */
+    wp_reset_postdata();
+
     endif;
 
     die();
